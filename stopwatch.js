f45837ac313f9b4bc616e5512ab835e9447aeb10
Chitrank Warokar <101242916+ChitrankC@users.noreply.github.com>
2023-02-01 10:47:10 +0530

    Add files via upload

---
+++ stopwatch.js
@@ -0,0 +1,49 @@
+window.onload = function () {
+    var seconds = 00;
+    var miliseconds = 00;
+    var append_miliseconds = document.getElementById("miliseconds");
+    var append_seconds = document.getElementById("seconds");
+    var btn_start = document.getElementById("btn_start");
+    var btn_stop  = document.getElementById("btn_stop");
+    var btn_reset = document.getElementById("btn_reset");
+    var Interval;
+
+
+
+    btn_start.onclick = function(){
+        clearInterval (Interval);
+        Interval = setInterval(startTimer, 10);
+    }
+    btn_stop.onclick = function(){
+        clearInterval(Interval);
+    }
+    btn_reset.onclick = function(){
+        clearInterval(Interval);
+        seconds = "00";
+        miliseconds ="00";
+        append_miliseconds.innerHTML = miliseconds;
+        append_seconds.innerHTML = seconds;
+    }
+    function startTimer(){
+        miliseconds++;
+        if(miliseconds<=9){
+            append_miliseconds.innerHTML = "0" + miliseconds;
+            
+        }
+        if(miliseconds>9){
+            append_miliseconds.innerHTML = miliseconds;
+        }
+        if(miliseconds>99){
+            console.log("seconds");
+            seconds++;
+            append_seconds.innerHTML = "0" + seconds;
+            miliseconds = 0;
+            append_miliseconds.innerHTML = "0" + 0;
+        }
+
+        if(seconds > 9){
+            append_seconds.innerHTML = seconds;
+        }
+
+    }
+}
